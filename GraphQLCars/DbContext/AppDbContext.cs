975bcec8a0f499557e1212232be90ca468537aaa
Latif Altay <latifaltay00@gmail.com>
2025-11-04 00:28:12 +0300

    feat(models): add DbContext

---
+++ GraphQLCars/DbContext/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GraphQLCars.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraphQLCars.HotChocolate.AspNetCoreDbContext;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+    {
+
+    }
+
+    public DbSet<Brand> Brands => Set<Brand>();
+    public DbSet<Car> Cars => Set<Car>();
+}
