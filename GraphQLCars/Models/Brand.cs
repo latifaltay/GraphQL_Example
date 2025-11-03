893de0c9ac304edda83c3961681672d3cbeebeb1
Latif Altay <latifaltay00@gmail.com>
2025-11-04 00:22:04 +0300

    feat(models): add entity models

---
+++ GraphQLCars/Models/Brand.cs
@@ -0,0 +1,8 @@
+﻿namespace GraphQLCars.Models;
+
+public class Brand
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public List<Car> Cars { get; set; } = new();
+}
---
+++ GraphQLCars/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQLCars.Models;
+
+public class Car
+{
+    public int Id { get; set; }
+    public string? Model { get; set; }
+    public int BrandId { get; set; }
+    public Brand? Brand { get; set; }
+}
